796ff913f1769d09580c143e80ce76eddb90f802
Kurt Riggin <krigscholar@gmail.com>
2025-10-16 15:52:04 -0600

    Begin to write tests for liveTreeAge function.

--- R/liveTreeAge.R
+++ R/liveTreeAge.R
@@ -1,10 +1,29 @@
-liveTreeAge <- function(data, Species, Age, DBH, Status, measYear){
+#' Predict unknown live tree ages from DBH of known live trees
+#'
+#' @param data Data with species, age, DBH, and tree status columns.
+#' @param speciesCol Species column in data.
+#' @param ageCol Age column in data.
+#' @param dbhCol DBH column in data.
+#' @param statusCol Tree Status column in data.
+#' @param measYear Year that data was measured.
+#'
+#' @returns Data with predicted ages for all live trees.
+#' @export
+#'
+#' @examples
+liveTreeAge <- function(data, speciesCol = Species, ageCol = Age, dbhCol = DBH, statusCol = Status, measYear){
+
+  # Pull out column names as strings
+  Species <- deparse(substitute(speciesCol))
+  Age <- deparse(substitute(ageCol))
+  DBH <- deparse(substitute(dbhCol))
+  Status <- deparse(substitute(statusCol))
 
   # Loop for each unique species in data
   for (sp in unique(data[[Species]])) {
 
     # Set LM to predict Age by DBH
-    formula <- as.formula(paste(Age, "~", DBH))
+    formula <- stats::as.formula(paste(Age, "~", DBH))
 
     # Create new data frame for live trees with know ages for LM
     speciesData <- data[data[[Species]] == sp &
@@ -12,15 +31,15 @@ liveTreeAge <- function(data, Species, Age, DBH, Status, measYear){
                           data[[Status]] == 1, ]
 
     # Fit LM for known trees (at least 2 needed per species)
-    if (nrow(speciesData) > 2) {
-      fit <- lm(formula, data = speciesData)
+    if (nrow(speciesData) > 1) {
+      fit <- stats::lm(formula, data = speciesData)
 
       # Grab live trees that have missing age data
-      missingRows <- is.na(data[[Age]]) & data[[Species]] == sp
+      missingRows <- is.na(data[[Age]]) & data[[Species]] == sp & data[[Status]] == 1
 
       # Fill in missing age data for live trees
       if (any(missingRows)) {
-        predictedAge <- predict(fit, newdata = data[missingRows, ])
+        predictedAge <- stats::predict(fit, newdata = data[missingRows, ])
         data[[Age]][missingRows] <- predictedAge
       }
     }

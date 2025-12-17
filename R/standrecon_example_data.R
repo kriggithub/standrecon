7315e23f7390e87cab8ab6c496f8c4b1e2eac7c7
Kurt Riggin <krigscholar@gmail.com>
2025-12-16 21:15:05 -0700

    Document synthetic dataset.

---
+++ R/standrecon_example_data.R
@@ -0,0 +1,14 @@
+#' Example field collected tree data for stand reconstruction
+#'
+#' A small synthetic data set for examples and tests.
+#'
+#'
+#' @format A dat aframe with 200 rows and 5 variables:
+#' \describe{
+#'    \item{Species}{Species code.}
+#'    \item{Age}{Tree age in years. Values that are known typically come from cored trees.}
+#'    \item{Status}{Tree status code.}
+#'    \item{Decay}{Decay class for dead trees.}
+#' }
+#' @source Simulated data set.
+"standrecon_example_data"

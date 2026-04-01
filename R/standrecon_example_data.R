115e276413b0149144fceb5c1e36524d3b3a66c6
Kurt Riggin <krigscholar@gmail.com>
2026-04-01 11:06:22 -0600

    Update example data documentation.

--- R/standrecon_example_data.R
+++ R/standrecon_example_data.R
@@ -3,12 +3,13 @@
 #' A small synthetic data set for examples and tests.
 #'
 #'
-#' @format A dat aframe with 200 rows and 5 variables:
+#' @format A data frame with 200 rows and 5 variables:
 #' \describe{
 #'    \item{Species}{Species code.}
 #'    \item{Age}{Tree age in years. Values that are known typically come from cored trees.}
 #'    \item{Status}{Tree status code.}
 #'    \item{Decay}{Decay class for dead trees.}
+#'    \item{DBH}{Diameter at breast height in cm.}
 #' }
 #' @source Simulated data set.
 "standrecon_example_data"
